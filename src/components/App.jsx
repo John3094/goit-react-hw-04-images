c74a0eb2db9c55c4a59723c15fd502ba79ce4df1
Yevhenii Yakovenko <yakovenkob@gmail.com>
2023-05-26 22:58:10 +0300

    start file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import { Component } from 'react';
+import clsx from 'clsx';
+import css from './';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>React homework template</div>;
 };
